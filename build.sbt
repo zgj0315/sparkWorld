6e4fbba6704cc98739fdc4b477f0a6ce540933d6
Zhao Guangjian <zgj0315@gmail.com>
2019-08-21 11:24:21 +0800

    message stat can work

--- build.sbt
+++ build.sbt
@@ -1,4 +1,4 @@
-import sbt.Keys._
+import sbt.Keys.{libraryDependencies, _}
 
 lazy val commonSettings = Seq(
   organization := "org.after90",
@@ -12,17 +12,17 @@ val spark_core = "org.apache.spark" % "spark-core_2.10" % "1.6.0"
 //val hadoop = "org.apache.hadoop" % "hadoop-client" % "2.7.3"
 //val scalactic = "org.scalactic" %% "scalactic" % "3.0.1"
 //val scalatest = "org.scalatest" % "scalatest_2.11" % "3.0.1" % "test"
-//val fastjson = "com.alibaba" % "fastjson" % "1.2.59"
+val fastjson = "com.alibaba" % "fastjson" % "1.2.59"
 
 lazy val root = (project in file(".")).
   settings(commonSettings: _*).
   settings(
     name := "SparkWorld",
-    libraryDependencies += spark_core
+    libraryDependencies += spark_core,
     //    libraryDependencies += spark_sql,
     //    libraryDependencies += spark_mllib,
     //    libraryDependencies += hadoop,
     //    libraryDependencies += scalactic,
     //    libraryDependencies += scalatest,
-//    libraryDependencies += fastjson
+    libraryDependencies += fastjson
   )
--- src/main/scala/org/after90/spark/EveriskData.scala
+++ src/main/scala/org/after90/spark/EveriskData.scala
@@ -1,10 +1,41 @@
 package org.after90.spark
 
+import com.alibaba.fastjson.JSON
+import org.apache.spark.{SparkConf, SparkContext}
+
 /**
   * 采用rdd的方式统计设备udid,imei数据
+  * spark-submit --jars fastjson-1.2.59.jar --class org.after90.spark.EveriskData sparkworld_2.10-0.1.0.jar local[*] /bangcle/bangcle_dataservice/data/message/message_data_20190621_1561079401417_1561079744157.gz /bangcle/zhaogj/output
+  * spark-submit --jars fastjson-1.2.59.jar --class org.after90.spark.EveriskData sparkworld_2.10-0.1.0.jar local[*] /bangcle/bangcle_dataservice/data/message/ /bangcle/zhaogj/output
   */
 object EveriskData {
   def main(args: Array[String]): Unit = {
-
+    var master = "local[*]"
+    var srcPath = "/Users/zhaogj/bangcle/everisk/src/message_data_20190805_1564969723942_1565231757484"
+    var outputPath = "/Users/zhaogj/tmp/output";
+    if (args.length == 3) {
+      master = args(0)
+      srcPath = args(1)
+      outputPath = args(2)
+    }
+    val conf = new SparkConf().setAppName("EveriskData").setMaster(master)
+    val sc = new SparkContext(conf)
+    val srcFile = sc.textFile(srcPath)
+    srcFile.map(_.split("\t"))
+      .filter(_.size == 3)
+      .map(x => JSON.parseObject(x(2).trim))
+      .map(x => {
+        var body = x.getJSONObject("body")
+        body.put("server_time", x.getLong("server_time"))
+        body
+      })
+      .filter(_.containsKey("udid"))
+      .map(x => {
+        (x.getString("udid"), 1)
+      })
+      .reduceByKey((x, y) => x + y)
+      .keys
+      .saveAsTextFile(outputPath)
+    sc.stop()
   }
 }
