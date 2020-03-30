1db8bcd4bc30ed5be57df1cb2e38fbd5eddd22e5
Zhao Guangjian <zgj0315@gmail.com>
2020-03-30 09:34:32 +0800

    graph reverse

--- README.md
+++ README.md
@@ -13,5 +13,5 @@ sbt package
 
 ## 启动命令
 ```
-spark-submit --jars fastjson-1.2.59.jar --class org.after90.everisk.ThreatData sparkworld_2.11-0.1.0.jar
+spark-submit --jars fastjson-1.2.59.jar --class org.after90.spark.WorldCount sparkworld_2.12-0.1.0.jar
 ```
--- src/main/scala/org/after90/spark/graphx/PropertyGraphExample.scala
+++ src/main/scala/org/after90/spark/graphx/PropertyGraphExample.scala
@@ -125,6 +125,13 @@ object PropertyGraphExample {
     ).collect.foreach(println(_))
     joinGraph.vertices.collect.foreach(println)
 
+    val graphR = graph.reverse
+    //show graph
+    graphR.triplets.map(triplet =>
+      "srcId:" + triplet.srcId + ", srcAttr:" + triplet.srcAttr + ", attr:" + triplet.attr + ", dstId:" + triplet.dstId + ", dstAttr:" + triplet.dstAttr
+    ).collect.foreach(println(_))
+    joinGraph.vertices.collect.foreach(println)
+
     sc.stop()
   }
 }
