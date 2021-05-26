ac797eb7ba1c00f2808d1cc456d833340cb96934
Zhao Guangjian <zgj0315@gmail.com>
2021-05-26 10:06:15 +0800

    json to csv, version update

--- build.sbt
+++ build.sbt
@@ -6,13 +6,13 @@ lazy val commonSettings = Seq(
   scalaVersion := "2.12.10"
 )
 
-val spark_core = "org.apache.spark" % "spark-core_2.12" % "2.4.5"
-val spark_sql = "org.apache.spark" % "spark-sql_2.12" % "2.4.5"
-val spark_mllib = "org.apache.spark" % "spark-mllib_2.12" % "2.4.5"
-val hadoop = "org.apache.hadoop" % "hadoop-client" % "2.7.3"
-val scalactic = "org.scalactic" %% "scalactic" % "3.0.1"
-val scalatest = "org.scalatest" % "scalatest_2.12" % "3.0.1" % "test"
-val fastjson = "com.alibaba" % "fastjson" % "1.2.59"
+val spark_core = "org.apache.spark" %% "spark-core" % "3.1.1"
+val spark_sql = "org.apache.spark" %% "spark-sql" % "3.1.1"
+val spark_mllib = "org.apache.spark" %% "spark-mllib" % "3.1.1"
+val hadoop = "org.apache.hadoop" % "hadoop-client" % "3.2.1"
+val scalactic = "org.scalactic" %% "scalactic" % "3.2.9"
+val scalatest = "org.scalatest" % "scalatest_2.12" % "3.2.9" % "test"
+val fastjson = "com.alibaba" % "fastjson" % "1.2.73"
 
 lazy val root = (project in file(".")).
   settings(commonSettings: _*).
--- src/main/scala/com/bangcle/everisk/Message.scala
+++ src/main/scala/com/bangcle/everisk/Message.scala
@@ -1,6 +1,6 @@
 package com.bangcle.everisk
 
-import com.alibaba.fastjson.JSON
+import com.alibaba.fastjson.{JSON, JSONObject}
 import org.apache.log4j.{Level, Logger}
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
@@ -9,7 +9,10 @@ import scala.collection.mutable.ArrayBuffer
 
 object Message {
 
-  case class Message(agent_id: String, udid: String, start_id: String, msg_type: String, msg: String)
+  //  case class Message(agent_id: String, udid: String, start_id: String, msg_type: String, msg: String)
+
+  case class Apk(agent_id: String, udid: String, pkg_name: String, app_name: String, ver_name: String,
+                 cert_md5: String, ver_code: String, cert_issure: String, md5: String)
 
   def main(args: Array[String]): Unit = {
     //屏蔽日志
@@ -28,32 +31,64 @@ object Message {
 
     import spark.implicits._
 
-    val msgFile = spark.sparkContext.textFile("/Volumes/SanDisk500G/message/201912/*20191212*.gz")
+    val msgFile = spark.sparkContext.textFile("/Users/zhaogj/tmp/*.gz")
     val msgDF = msgFile
       .map(_.split("\t"))
       .filter(_.size == 3)
       .map(x => (x(0).trim, JSON.parseObject(x(2).trim)))
       .filter(_._2.containsKey("body"))
       .map(x => (x._1, x._2.getJSONObject("body")))
+      .filter(x => {
+        x._2.containsKey("protol_type") &&
+          x._2.containsKey("apk_flag") &&
+          x._2.containsKey("data") &&
+          x._2.getString("protol_type").equals("apkinfo") &&
+          (x._2.getString("apk_flag").equals("all") ||
+            x._2.getString("apk_flag").equals("update")
+            )
+      })
       .map(f = x => {
-        var list = ArrayBuffer[Message]()
-        list += Message(x._2.getString("agent_id"), x._2.getString("udid"), x._2.getString("start_id"),
-          "ip_src", x._1)
-        if (x._2.containsKey("wifi_info")) {
-          val listWifi = x._2.getJSONArray("wifi_info")
-          for (i <- 0 until listWifi.size()) {
-            val wifi = listWifi.getJSONObject(i)
-            list += Message(x._2.getString("agent_id"), x._2.getString("udid"), x._2.getString("start_id"),
-              "wifi", wifi.getString("ssid") + ":" + wifi.getString("bssid"))
+        var list = ArrayBuffer[Apk]()
+        //        list += Apk(x._2.getString("agent_id"), x._2.getString("udid"), x._2.getString("start_id"),
+        //          "ip_src", x._1)
+        //        if (x._2.getString("apk_flag").equals("update") && x._2.containsKey("data")) {
+        val agent_id = x._2.getString("agent_id");
+        val udid = x._2.getString("udid");
+        val data = x._2.getJSONArray("data");
+
+        for (i <- 0 until data.size()) {
+          val json = data.getJSONObject(i)
+          var app_name = json.getString("app_name")
+          if (app_name != null) {
+            app_name = app_name
+              .replace("\n", "")
+              .replace("\t", "");
           }
+          var cert_issure = json.getString("cert_issure");
+          if (cert_issure != null) {
+            cert_issure = cert_issure
+              .replace("\n", "")
+              .replace("\t", "");
+          }
+          list += Apk(agent_id, udid, json.getString("pkg_name"), app_name,
+            json.getString("ver_name"), json.getString("cert_md5"), json.getString("ver_code"),
+            cert_issure, json.getString("md5"))
         }
+
+        //        }
         list
       })
       .flatMap(x => x)
       .toDF()
-    msgDF.createOrReplaceTempView("msg")
-    var sqlDF = spark.sql("SELECT COUNT(*) FROM msg")
-    sqlDF.show()
+    msgDF.createOrReplaceTempView("apk")
+    var sqlDF = spark.sql("SELECT distinct * FROM apk")
+    //    sqlDF.show(1000)
+    sqlDF
+      .repartition(1)
+      .write
+      .option("delimiter", "\t")
+      .csv("/Users/zhaogj/tmp/apk")
+
     //    var sqlDF = spark.sql("SELECT * FROM msg")
     //    sqlDF.show(1000)
 
